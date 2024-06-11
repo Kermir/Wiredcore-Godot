e18be936c29601e6c6c7800ab02a6e641e27ed33
Ramker <136987349+Kermir@users.noreply.github.com>
2024-06-11 20:53:44 +0000

    Update player.gd
    I fixed my shit with anim.flip_h so there are no more crash but side_walk is still broken

--- scripts/player.gd
+++ scripts/player.gd
@@ -32,29 +32,29 @@ func player_movement(delta):
 		
 func play_anim(movement):
 	var dir = current_dir
-	var anim = $AnimatedSprite2D
+	var anim = $CollisionShape2D/AnimatedSprite2D
 	
 	if dir == "right":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("side_walk")
-		elif movement == 0:
+		else:
 			anim.play("side_idle")
 	if dir == "left":
 		anim.flip_h = true
 		if movement == 1:
-			anim.play("side_walk2")
-		elif movement == 0:
-			anim.play("side_idle2")
+			anim.play("side_walk")
+		else:
+			anim.play("side_idle")
 	
 	if dir == "down":
-		anim.flip_h = true
+		# anim.flip_h = true
 		if movement == 1:
 			anim.play("front_walk")
 		elif movement == 0:
 			anim.play("front_idle")
 	if dir == "up":
-		anim.flip_h = true
+		# anim.flip_h = true
 		if movement == 1:
 			anim.play("back_walk")
 		elif movement == 0:
